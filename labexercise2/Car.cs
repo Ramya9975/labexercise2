d7457639b9cfdbfdd1ed334195bc44b16c939450
Ramyamani9975 <ramyamanivannan99@gmail.com>
2023-02-01 17:15:14 +0530

    Add project files.

---
+++ labexercise2/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labexercise2
+{
+    internal class Car
+    {
+        string Engine;
+        int NoOfWheels;
+
+        public void AcceptDetails()
+        {
+            Console.WriteLine("Enter the Engine Model");
+            Engine = Console.ReadLine();
+            Console.WriteLine("Enter the number of Wheels");
+            NoOfWheels = Convert.ToInt32(Console.ReadLine());
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("The Engine model is:{0}", Engine);
+            Console.WriteLine("The number of wheels are:{0}", NoOfWheels);
+        }
+    }
+}
+
+
+            
+
---
+++ labexercise2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labexercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car= new Car();
+            car.AcceptDetails();
+            car.DisplayDetails();
+            Console.ReadLine();
+        }
+    }
+}
